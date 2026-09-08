54bf9e5858d78d7d2a58bc16c74020c9882f8fdc
mac-mac-sg <marco.raith@outlook.com>
2026-09-08 11:22:39 +0200

    Redesign-Styles laden

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { App } from './App'
 import './styles.css'
+import './redesign.css'
 
 const wurzel = document.getElementById('root')
 if (!wurzel) throw new Error('Wurzelelement #root fehlt')
