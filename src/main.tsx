58b9f416b591e43ed45d5fe18882c6d514fc8bbf
mac-mac-sg <marco.raith@outlook.com>
2026-09-08 19:48:07 +0200

    Scanner-Stile einbinden

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import './styles.css'
 import './redesign.css'
 import './redesign-fixes.css'
 import './wissen.css'
+import './scanner.css'
 
 const wurzel = document.getElementById('root')
 if (!wurzel) throw new Error('Wurzelelement #root fehlt')
