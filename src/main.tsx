27f69cf7d052fde9ef0594d6dd045b3011b5da83
mac-mac-sg <marco.raith@outlook.com>
2026-09-08 11:23:09 +0200

    Redesign-Kontextkorrekturen laden

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import { App } from './App'
 import './styles.css'
 import './redesign.css'
+import './redesign-fixes.css'
 
 const wurzel = document.getElementById('root')
 if (!wurzel) throw new Error('Wurzelelement #root fehlt')
